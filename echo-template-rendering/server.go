3a5a42f0b21e8b115b04a5d656841f6bff336a25
suzunedev <suzunedev@gmail.com>
2022-07-11 02:01:02 +0900

    Advanced - Calling Echo from templates

--- echo-template-rendering/server.go
+++ echo-template-rendering/server.go
@@ -13,11 +13,13 @@ type Template struct {
 }
 
 func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
-	return t.templates.ExecuteTemplate(w, name, data)
-}
 
-func Hello(c echo.Context) error {
-	return c.Render(http.StatusOK, "hello", "World")
+	// Add global methods if data is a map
+	if viewContext, isMap := data.(map[string]interface{}); isMap {
+		viewContext["reverse"] = c.Echo().Reverse
+	}
+
+	return t.templates.ExecuteTemplate(w, name, data)
 }
 
 func main() {
@@ -26,7 +28,13 @@ func main() {
 	}
 	e := echo.New()
 	e.Renderer = t
-	e.GET("/hello", Hello)
+
+	// Named route "foobar"
+	e.GET("/something", func(c echo.Context) error {
+		return c.Render(http.StatusOK, "template.html", map[string]interface{}{
+			"name": "Dolly!",
+		})
+	}).Name = "foobar"
 
 	e.Logger.Fatal(e.Start(":8080"))
 }
