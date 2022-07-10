61d3db970ac5faa86559dd67f1523c15b28878dc
suzunedev <suzunedev@gmail.com>
2022-07-11 01:52:20 +0900

    Add Template Rendering

---
+++ echo-template-rendering/server.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"text/template"
+
+	"github.com/labstack/echo"
+)
+
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func Hello(c echo.Context) error {
+	return c.Render(http.StatusOK, "hello", "World")
+}
+
+func main() {
+	t := &Template{
+		templates: template.Must(template.ParseGlob("public/views/*.html")),
+	}
+	e := echo.New()
+	e.Renderer = t
+	e.GET("/hello", Hello)
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
