2684e35c221cd945a8d3970fe3f686cf7c6299c0
suzunedev <suzunedev@gmail.com>
2022-07-10 10:41:18 +0900

    Add query parameters

--- echo-practice/main.go
+++ echo-practice/main.go
@@ -18,6 +18,7 @@ func main() {
 	// Routes
 	e.GET("/", hello)
 	e.GET("users/:id", getUser)
+	e.GET("/show", show)
 
 	// Start server
 	e.Logger.Fatal(e.Start(":1323"))
@@ -33,4 +34,12 @@ func getUser(c echo.Context) error {
 	// User ID from path `users/:id`
 	id := c.Param("id")
 	return c.String(http.StatusOK, id)
+}
+
+// Query Parameters
+func show(c echo.Context) error {
+	// Get team and member from the query string
+	team := c.QueryParam("team")
+	member := c.QueryParam("member")
+	return c.String(http.StatusOK, "team:"+team+", member:"+member)
 }
