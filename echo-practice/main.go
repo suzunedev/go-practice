4c2e9e68d55c51119d05e71d63b4f400a7c3bd78
suzunedev <suzunedev@gmail.com>
2022-07-10 18:06:41 +0900

    Add Middleware

--- echo-practice/main.go
+++ echo-practice/main.go
@@ -36,6 +36,26 @@ func main() {
 	})
 	e.Static("/static", "static")
 
+	// Group level middleware
+	g := e.Group("/admin")
+	g.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
+		if username == "joe" && password == "secret" {
+			return true, nil
+		}
+		return false, nil
+	}))
+
+	// Route level middleware
+	track := func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			println("request to /users")
+			return next(c)
+		}
+	}
+	e.GET("/users", func(c echo.Context) error {
+		return c.String(http.StatusOK, "/users")
+	}, track)
+
 	// Start server
 	e.Logger.Fatal(e.Start(":1323"))
 }
