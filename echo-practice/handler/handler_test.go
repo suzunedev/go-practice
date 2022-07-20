c9c9b1997ea4bb113ba4c0847c83eb045a6e1967
suzunedev <suzunedev@gmail.com>
2022-07-20 23:40:06 +0900

    Add Testing Handler

---
+++ echo-practice/handler/handler_test.go
@@ -0,0 +1,60 @@
+package handler
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	mockDB = map[string]*User{
+		"jon@labstack.com": {"Jon Snow", "jon@labstack.com"},
+	}
+	userJSON = `{"name":"Jon Snow","email":"jon@labstack.com"}`
+)
+
+func TestCreateUser(t *testing.T) {
+	// Setup
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodPost, "/", strings.NewReader(userJSON))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	h := &handler{mockDB}
+
+	// Assertions
+	if assert.NoError(t, h.createUser(c)) {
+		assert.Equal(t, http.StatusCreated, rec.Code)
+		expected := User{"Jon Snow", "jon@labstack.com"}
+		b := new(bytes.Buffer)
+		json.NewEncoder(b).Encode(expected)
+		assert.Equal(t, b.String(), rec.Body.String())
+	}
+}
+
+func TestGetUser(t *testing.T) {
+	// Setup
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	c.SetPath("/users/:email")
+	c.SetParamNames("email")
+	c.SetParamValues("jon@labstack.com")
+	h := &handler{mockDB}
+
+	// Assertions
+	if assert.NoError(t, h.getUser(c)) {
+		assert.Equal(t, http.StatusOK, rec.Code)
+		expected := User{"Jon Snow", "jon@labstack.com"}
+		b := new(bytes.Buffer)
+		json.NewEncoder(b).Encode(expected)
+		assert.Equal(t, b.String(), rec.Body.String())
+	}
+}
